7b228d913d7c7722cd5ef5d4627d2d56d6d75108
andregustavocrtb <63739467+andregustavocrtb@users.noreply.github.com>
2022-09-04 19:49:29 -0300

    Adicionando arquivos

---
+++ app.js
@@ -0,0 +1,12 @@
+document.getElementById("hobbies").onclick = function(){
+    location.href="hobbies.html"
+}
+
+document.getElementById("conq-acad").onclick = function(){
+    location.href="academics.html"
+}
+
+document.getElementById("pro").onclick = function(){
+    location.href="professional.html"
+}
+
