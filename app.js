112009417b3c1a9ab54358c253f78138a9b741e0
andregustavocrtb <63739467+andregustavocrtb@users.noreply.github.com>
2022-09-04 19:57:40 -0300

    Alterando a posicao das paginas

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 document.getElementById("hobbies").onclick = function(){
-    location.href="hobbies.html"
+    location.href="./pages/hobbies.html"
 }
 
 document.getElementById("conq-acad").onclick = function(){
-    location.href="academics.html"
+    location.href="./pages/academics.html"
 }
 
 document.getElementById("pro").onclick = function(){
-    location.href="professional.html"
+    location.href="./pages/professional.html"
 }
 
